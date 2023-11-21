8f6fd721f75f62c10c805ed82958339d0f2d73a1
wzseba <wzseba@gmail.com>
2023-11-20 21:09:54 -0300

    add: nuevo metodo para buscar un libro

--- src/bibliotecaNoche/Biblioteca.java
+++ src/bibliotecaNoche/Biblioteca.java
@@ -1,46 +1,57 @@
 package bibliotecaNoche;
 
-import java.util.Iterator;
-
 public class Biblioteca {
-	
+
 	private Libro[] libros;
 	private int cantidadDeLibros = 0;
-	
-	//constructor array
-	public Biblioteca (int maxCantidadDeLibros) {
+
+	// constructor array
+	public Biblioteca(int maxCantidadDeLibros) {
 		libros = new Libro[maxCantidadDeLibros];
 	}
-	
-	public void agregarLibro(String titulo, String autor, Genero genero, int paginas){
-		if(cantidadDeLibros >= libros.length) {
+
+	public void agregarLibro(String titulo, String autor, Genero genero, int paginas) {
+		if (cantidadDeLibros >= libros.length) {
 			throw new Error("biblioteca completa");
 		}
-		
-	//si hay espacio agrego el nuevo libro
+
+		// si hay espacio agrego el nuevo libro
 		Libro libroNuevo = new Libro(titulo, autor, genero, paginas);
 		libros[cantidadDeLibros] = libroNuevo;
 		cantidadDeLibros++;
 	}
-	//sobre carga de metodos
-	public void agregarLibro(Libro libro){
-		if(cantidadDeLibros >= libros.length) {
+
+	// sobre carga de metodos
+	public void agregarLibro(Libro libro) {
+		if (cantidadDeLibros >= libros.length) {
 			throw new Error("Biblioteca completa");
 		}
-		
+
 		libros[cantidadDeLibros] = libro;
 		cantidadDeLibros++;
 	}
-	
-	public int cuantosLibros(){
+
+	// indicar si contiene un libro dado
+	public boolean contieneElLibro(Libro libroBuscado) {
+
+		for (Libro libro : libros) {
+			if (libro != null && libro.equals(libroBuscado)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public int cuantosLibros() {
 		return cantidadDeLibros;
 	}
-	
-	public String libroEnLaPosicion(int posicion){
+
+	public String libroEnLaPosicion(int posicion) {
 		return libros[posicion - 1].getTitulo();
 	}
-	
-	public Libro libroConMasPaginas(){
+
+	public Libro libroConMasPaginas() {
 		int posicionDelLibroConMasPaginas = 0;
 
 		for (int i = 1; i < cantidadDeLibros; i++) {
